e973be850891b3d7feedb90c0e77d1250b1b0727
SIVANAGARAJ <asiva_pvl@yahoo.co.in>
2019-06-06 15:55:58 +0530

    Add files via upload

--- Connection from python to sql.py
+++ Connection from python to sql.py
@@ -6,16 +6,23 @@ Created on Thu Jun  6 11:26:16 2019
 """
 
 import pyodbc
+import sqlalchemy
 import pandas as pd
 connection=pyodbc.connect('Driver={SQL Server Native Client 11.0};'
                 'Server=SSINTR02;'
                 'Database=T5_Vijayakumar;'
                 'uid=T5_Vijayakumar;'
                 'pwd=vijay207$;')
-sql='select * from customer'
+
+engine=sqlalchemy.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(connection))
+
+sql='select * from test101'
 df=pd.read_sql(sql,connection)
 print(df)
+
+
 td=pd.DataFrame(df)
 print(td)
 
-#df.to_sql('test6',con=engine,if_exists='replace',index=False)
+df.to_sql('test101',con=engine,if_exists='replace',index=False)
+
